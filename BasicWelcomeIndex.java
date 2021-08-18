eb0db4db26de2bd00c55fd165b90293b80a9a406
mrunal08 <mrunalg96@gmail.com>
2021-08-18 08:11:20 +0530

    First COmmit

---
+++ BasicWelcomeIndex.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BasicWelcomeIndex {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Functional Programs");
+        System.out.println("UC1 : 2D Array");
+        System.out.println("UC2 : Sum of Three Integer");
+        System.out.println("UC3 : Distance Program");
+        System.out.println("UC4 : Quadratic Equation");
+        System.out.println("UC5 : Windchill Program");
+    }
+}
